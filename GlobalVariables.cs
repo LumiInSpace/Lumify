1d052fb7a6eb72a11d5121cdc8cad958d9da86ef
LumiInSpace <finrichter@gmail.com>
2025-12-04 19:23:26 +0100

    :sparkles: 0.2.3.4 added global variables

---
+++ GlobalVariables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lumify
+{
+    public class GlobalVariables
+    {
+        public const string AppName = "Lumify";
+        public const string Version = ""; //in development
+
+        //Path
+        public static readonly string BaseDirectory =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Lumify");
+
+        //public static readonly string ConfigPath =
+        //    Path.Combine(BaseDirectory, "config.json");
+
+        //public static readonly string LogDirectory =
+        //    Path.Combine(BaseDirectory, "logs");
+
+        public static readonly string MaterialListPath =
+            Path.Combine(BaseDirectory, "Lists");
+    }
+}
